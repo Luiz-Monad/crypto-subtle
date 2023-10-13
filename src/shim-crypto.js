ffa38b56e4237842f78ddb85c678b63a49d91272
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2023-10-13 12:28:32 -0500

    shim: removed need for node assert

--- src/shim-crypto.js
+++ src/shim-crypto.js
@@ -1,18 +1,27 @@
-
 function randomBytes(length) {
-    (0, index_js_1.assert)(window.crypto != null, "platform does not support secure random numbers", "UNSUPPORTED_OPERATION", {
-        operation: "randomBytes"
-    });
-    (0, index_js_1.assertArgument)(Number.isInteger(length) && length > 0 && length <= 1024, "invalid length", "length", length);
-    const result = new Uint8Array(length);
-    window.crypto.getRandomValues(result);
-    return result;
+  if (typeof window === 'undefined' || !window.crypto) {
+    throw new Error('Platform does not support secure random numbers');
+  }
+  if (!Number.isInteger(length) || length <= 0 || length > 1024) {
+    throw new Error('Invalid length');
+  }
+  const result = new Uint8Array(length);
+  window.crypto.getRandomValues(result);
+  return result;
 }
+
 exports.randomBytes = randomBytes;
 
 function createHmac(_algo, key) {
-    const algo = ({ sha256: sha256_1.sha256, sha512: sha512_1.sha512 }[_algo]);
-    (0, index_js_1.assertArgument)(algo != null, "invalid hmac algorithm", "algorithm", _algo);
-    return hmac_1.hmac.create(algo, key);
+  const supportedAlgorithms = {
+    sha256: sha256_1.sha256,
+    sha512: sha512_1.sha512,
+  };
+  const algo = supportedAlgorithms[_algo];
+  if (!algo) {
+    throw new Error('Invalid hmac algorithm');
+  }
+  return hmac_1.hmac.create(algo, key);
 }
+
 exports.createHmac = createHmac;
